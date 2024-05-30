b1852bda7f54a3d09f0d6b612174fbc19fb70fb6
EdwinLF <117051836+EdwinLF@users.noreply.github.com>
2024-05-30 13:55:14 -0600

    cambiios en el react y php
    hola como esta ntodos

--- api_php/services/AdmistrativoService.php
+++ api_php/services/AdmistrativoService.php
@@ -1,9 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 require_once '../controller/AdministrativoController.php';  
 $controller = new AdministrativoController();
-if(isset($_GET['id'])){
-    
-    echo json_encode("hola");
+if(isset($_GET['idEmpresa'])){
+    $empresa = $controller->getEmpresaById($_GET['idEmpresa']);
+    echo json_encode($empresa);
+}else if(isset($_GET['idEmpresaCupon'])){
+    $cupones = $controller->getCuponesByIdEmpresa($_GET['idEmpresaCupon']);
+    echo json_encode($cupones);
 }else{
     $empresas = $controller->getEmpresasActivas();
     echo json_encode($empresas);
